25c49228ec013e3d2985536ee345c1720c9924a4
smilu97 <smilup2244@gmail.com>
2016-07-24 01:24:30 +0900

    Add some tester in schema

--- schema.sql
+++ schema.sql
@@ -25,3 +25,11 @@ create table posts(
     content string,
     files string not null
 );
+
+insert into users(name, id, password) values ('tester1', 'test1', '1234');
+insert into users(name, id, password) values ('tester2', 'test2', '1234');
+insert into users(name, id, password) values ('tester3', 'test3', '1234');
+
+insert into posts(title, content, files) values ('test post1', 'test content', '');
+insert into posts(title, content, files) values ('test post2', 'test content', '');
+insert into posts(title, content, files) values ('test post3', 'test content', '');
